20b001fb96e2df53d72785052609f88efab260bc
Atharv Pandit <atharvpandit@Atharvs-Air.lan>
2022-10-02 22:14:54 -0400

    Made changes to driver.py json format

--- src/driver.py
+++ src/driver.py
@@ -1,15 +1,17 @@
 import json
 import disk
-from getmac import get_mac_address as gma
 import os
 import yaml
-import json
+import sys 
+import time
 from elasticsearch import Elasticsearch
+from getmac import get_mac_address as gma
 
-
+'''
+Client Code integrates all metrics and send to Elastic Server
+'''
 
 if __name__ == "__main__":
-    
     client_json = {}
 
 # read config from yml file
@@ -17,22 +19,19 @@ if __name__ == "__main__":
         config = yaml.safe_load(file)
 
 # collect data
-    client_json["version"] = config["version"]
 
-# push json object to elastic backend
+    version = config["version"]
+    client_json["metadata"]={"version":version,"time":str(time.localtime)}
     token = (config["auth"]["username"], config["auth"]["password"])
-    
     client_id=str((gma()))
     client_json["id"]=client_id
     client_disk=disk.Disk()
     client_disk_info=client_disk.retrieve_disk_info()
     client_json["Disk"]=client_disk_info
-    
-
-
-
-
-    client_json=json.dumps(client_json)
+    #More code for other metrics
+    # print(client_json)
+    client_json=json.dumps(client_json,indent = 2)
+    # print(client_json)
     es = Elasticsearch(hosts = config["connect"]["endpoint"], ssl_assert_fingerprint = config["connect"]["tls-fingerprint"], basic_auth = token)
     resp = es.index(index = config["index"]["name"], document = client_json)
-    # print(client_json)
+    print(client_json)
