4daccb8c11577e27993270bd3669bef61a79b148
vsangarya <vishwesharya@gmail.com>
2022-10-02 20:00:13 -0400

    Edit disk.py

--- code/disk.py
+++ code/disk.py
@@ -6,25 +6,35 @@ class Disk:
   def __init__(self):
     self.data = {}
 
+
+  def checkAttr(self,attr, val):
+    try:
+      val = getattr(attr, val)
+    except:
+      val = None
+    return val
+
+
+
   def retrieve_disk_info(self):
     par = psutil.disk_partitions()
     self.data["Disk"] = []
     for x in par:
       dsk = psutil.disk_usage(x.mountpoint)
       each_disk = {}
-      each_disk["name"] = x.device if hasattr(x,"device") else None
-      each_disk["type"] = x.fstype if hasattr(x,"fstype") else None
-      each_disk["total_size"] = dsk.total if hasattr(dsk,"total") else None
-      each_disk["used"] = dsk.used if hasattr(dsk,"used") else None
-      each_disk["free"] = dsk.free if hasattr(dsk,"free") else None
-      each_disk["percetage"] = dsk.percent if hasattr(dsk,"percent") else None
-      
-      self.data["Disk"].append(each_disk) 
-    return self.data
 
- 
+      each_disk["name"] = self.checkAttr(x,"device")
+      each_disk["type"] = self.checkAttr(x,"fstype")
+      each_disk["total_size"] = self.checkAttr(dsk,"total")
+      each_disk["used"] = self.checkAttr(dsk,"used")
+      each_disk["free"] = self.checkAttr(dsk,"free")
+      each_disk["percetage"] = self.checkAttr(dsk,"percent")
+
+      self.data["Disk"].append(each_disk)
+    return json.dumps(self.data, indent = 2)
+
+"""
 if __name__ == "__main__":
   d = Disk()
-  disk_data = json.dumps(d.retrieve_disk_info(), indent = 2)
-  #print(disk_data)
-
+  print(d.retrieve_disk_info())
+"""
