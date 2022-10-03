045712c1cecd06341be79bc5c390641f65801e2e
Vishwesh Sangarya <68890508+VSangarya@users.noreply.github.com>
2022-10-02 20:17:27 -0400

    Update disk.py

--- src/disk.py
+++ src/disk.py
@@ -1,37 +1,38 @@
+"""This module fetches disk information and returns a JSON with the disk data"""
 import psutil
 import json
 
 
 class Disk:
-  def __init__(self):
-    self.data = {}
+    """Class to fetch, format and return disk information"""
+    def __init__(self):
+        self.data = {}
 
 
-  def checkAttr(self,attr, val):
-    try:
-      val = getattr(attr, val)
-    except:
-      val = None
-    return val
-
+    def check_attr(self,attr, val):
+        try:
+            val = getattr(attr, val)
+        except:
+            val = None
+        return val
 
 
   def retrieve_disk_info(self):
-    par = psutil.disk_partitions()
-    self.data["Disk"] = []
-    for x in par:
-      dsk = psutil.disk_usage(x.mountpoint)
-      each_disk = {}
+      par = psutil.disk_partitions()
+      self.data["Disk"] = []
+      for x in par:
+          dsk = psutil.disk_usage(x.mountpoint)
+          each_disk = {}
 
-      each_disk["name"] = self.checkAttr(x,"device")
-      each_disk["type"] = self.checkAttr(x,"fstype")
-      each_disk["total_size"] = self.checkAttr(dsk,"total")
-      each_disk["used"] = self.checkAttr(dsk,"used")
-      each_disk["free"] = self.checkAttr(dsk,"free")
-      each_disk["percetage"] = self.checkAttr(dsk,"percent")
+          each_disk["name"] = self.check_attr(x,"device")
+          each_disk["type"] = self.check_attr(x,"fstype")
+          each_disk["total_size"] = self.check_attr(dsk,"total")
+          each_disk["used"] = self.check_attr(dsk,"used")
+          each_disk["free"] = self.check_attr(dsk,"free")
+          each_disk["percetage"] = self.check_attr(dsk,"percent")
 
-      self.data["Disk"].append(each_disk)
-    return json.dumps(self.data, indent = 2)
+          self.data["Disk"].append(each_disk)
+      return json.dumps(self.data, indent = 2)
 
 """
 if __name__ == "__main__":
