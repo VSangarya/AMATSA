c83ab73a7bbb2e495fe5745e50053852ef3286f6
vsangarya <vishwesharya@gmail.com>
2022-10-04 14:17:48 -0400

    Edit disk.py, lintr isue fix

--- src/disk.py
+++ src/disk.py
@@ -3,7 +3,6 @@ import psutil
 from utils import size_in_gb
 from system import System
 import re
-import json
 
 class Disk:
     """Class to fetch, format and return disk information"""
@@ -23,31 +22,31 @@ class Disk:
     def format_darwin(self, data):
         disk_nums = [ [] for i in range(len(data["disk"]))]
         for i in range(0,len(disk_nums)):
-           result = re.search("/dev/disk(\d+)s", data["disk"][i]["name"])
-           if result is not None:
-            result = int(result.group(0).replace('/dev/disk','').replace('s',''))
-            data["disk"][i]["number"] = result
-            disk_nums[result].append(data["disk"][i])
+            result = re.search("/dev/disk(\d+)s", data["disk"][i]["name"])
+            if result is not None:
+                result = int(result.group(0).replace("/dev/disk","").replace("s",""))
+                data["disk"][i]["number"] = result
+                disk_nums[result].append(data["disk"][i])
     
         res = [ele for ele in disk_nums if ele != []]
 
         final_disk = []
-        for eachDisk in res:
-            print(eachDisk)
-            total_size = eachDisk[0]['total_size']
-            used = eachDisk[0]['used']
-            free = eachDisk[0]['free']
-            name = "/dev/disk"+str(eachDisk[0]["number"])
-            type = eachDisk[0]["type"]
+        for each_disk in res:
+            print(each_disk)
+            total_size = each_disk[0]["total_size"]
+            used = each_disk[0]["used"]
+            free = each_disk[0]["free"]
+            name = "/dev/disk"+str(each_disk[0]["number"])
+            type = each_disk[0]["type"]
             percentage = round((used/total_size)*100,2)
 
             summed_disk ={
-                'name':name,
-                'type': type,
-                'total_size' : total_size,
-                'used' : used,
-                'free' : free,
-                'percentage' : percentage
+                "name" : name,
+                "type" : type,
+                "total_size" : total_size,
+                "used" : used,
+                "free" : free,
+                "percentage" : percentage
             }
 
             final_disk.append(summed_disk)
@@ -71,7 +70,7 @@ class Disk:
             self.data["disk"].append(each_disk)
 
 
-        if(self.sys.platform_name == "Darwin"):
+        if self.sys.platform_name == "Darwin":
             self.data["disk"] = self.format_darwin(self.data)
 
         return self.data["disk"]
