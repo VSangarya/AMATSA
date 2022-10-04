b9b7f7d02ddbf5232eaa99b9178bcdb794cffda8
vsangarya <vishwesharya@gmail.com>
2022-10-04 14:03:26 -0400

    Edit disk.py

--- src/disk.py
+++ src/disk.py
@@ -1,11 +1,15 @@
 """This module fetches disk information and returns a JSON with the disk data"""
 import psutil
 from utils import size_in_gb
+from system import System
+import re
+import json
 
 class Disk:
     """Class to fetch, format and return disk information"""
     def __init__(self):
         self.data = {}
+        self.sys = System()
 
 
     def check_attr(self,attr, val):
@@ -14,6 +18,40 @@ class Disk:
         except: # pylint: disable=bare-except
             val = None
         return val
+
+
+    def format_darwin(self, data):
+        disk_nums = [ [] for i in range(len(data["disk"]))]
+        for i in range(0,len(disk_nums)):
+           result = re.search("/dev/disk(\d+)s", data["disk"][i]["name"])
+           if result is not None:
+            result = int(result.group(0).replace('/dev/disk','').replace('s',''))
+            data["disk"][i]["number"] = result
+            disk_nums[result].append(data["disk"][i])
+    
+        res = [ele for ele in disk_nums if ele != []]
+
+        final_disk = []
+        for eachDisk in res:
+            print(eachDisk)
+            total_size = eachDisk[0]['total_size']
+            used = eachDisk[0]['used']
+            free = eachDisk[0]['free']
+            name = "/dev/disk"+str(eachDisk[0]["number"])
+            type = eachDisk[0]["type"]
+            percentage = round((used/total_size)*100,2)
+
+            summed_disk ={
+                'name':name,
+                'type': type,
+                'total_size' : total_size,
+                'used' : used,
+                'free' : free,
+                'percentage' : percentage
+            }
+
+            final_disk.append(summed_disk)
+        return final_disk
 
 
     def retrieve_disk_info(self):
@@ -31,4 +69,10 @@ class Disk:
             each_disk["percentage"] = self.check_attr(dsk,"percent")
 
             self.data["disk"].append(each_disk)
+
+
+        if(self.sys.platform_name == "Darwin"):
+            self.data["disk"] = self.format_darwin(self.data)
+
         return self.data["disk"]
+
