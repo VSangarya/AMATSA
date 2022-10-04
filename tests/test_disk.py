e1a77dd64e8bf8f6ebb07cb6ca77fe20b79a6a68
vsangarya <vishwesharya@gmail.com>
2022-10-04 16:33:04 -0400

    Add disk partition summarizing test

--- tests/test_disk.py
+++ tests/test_disk.py
@@ -1,4 +1,5 @@
 """Tests for the Disk module"""
+from pandas import DataFrame
 from src.disk import Disk
 
 def test_disk_init():
@@ -33,14 +34,29 @@ def test_disk_values():
     try:
         disk = Disk()
         data = disk.retrieve_disk_info()
-
-        fields = ["total_size", "used", "free", "percentage"]
-        for each in data:
-            for each_field in fields:
-                if each[each_field] < 0:
-                    print(f"For disk data '{each_field}', value cannot be negative")
-                    success = False
     except: # pylint: disable=bare-except
         print("Failed to collect Data metrics")
+        success = False
+    fields = ["total_size", "used", "free", "percentage"]
+    for each in data:
+        for each_field in fields:
+            if each[each_field] < 0:
+                print(f"For disk data '{each_field}', value cannot be negative")
+                success = False
+    assert success
+
+
+def test_disk_darwin_sum():
+    success = True
+    to_summarize_data = {"disk" : [{"name": "/dev/disk1s1s1", "type": "apfs", "total_size": 10, "used": 2, "free": 8, "percentage": 2}, 
+                                    { "name": "/dev/disk1s2", "type": "apfs", "total_size": 10, "used": 2, "free": 8, "percentage": 2}]}
+    data_to_be_returned = [{"name": "/dev/disk1", "type": "apfs", "total_size": 10, "used": 2, "free": 8, "percentage": 20.0}]
+    try:
+        disk = Disk()
+        data = disk.format_darwin(to_summarize_data)
+        if data != data_to_be_returned:
+            print("format_darwin() not function as desired")
+            success = False
+    except : # pylint: disable=bare-except
         success = False
     assert success
