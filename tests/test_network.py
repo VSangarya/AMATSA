2f70791596f871c24c2851a4e398396b6417000e
Shivangi Chopra <shivangichopra10@gmail.com>
2022-10-06 19:15:23 -0400

    Adding test modules for network

--- tests/test_network.py
+++ tests/test_network.py
@@ -25,3 +25,20 @@ def test_network_entries():
     except:
         success = False
     assert success, "Failed to collect Network info"
+
+def test_network_datatype():
+    success = True
+    json = {}
+    try:
+        net_obj = Network()
+        net_obj.get_network_info()
+        net_obj.fill_network_info(json)
+    except:
+        print("Failed to collect Network metrics")
+        success = False
+    data_types = {"mac_address" : str, "ip_address" : str, "hostname" : str, "connection_status" : bool, "down_speed" : float, "up_speed" : float, "time_now" : str}
+    for k, v in json.items():
+        if not isinstance(v, data_types[k]) :
+            print(f"For data '{k}', type should be {data_types[k]}, not {type(v[k])}")
+            success = False
+    assert success
