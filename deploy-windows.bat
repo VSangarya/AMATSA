0e6f008365ebed85f1a766931b72e6dd803c62b9
vsangarya <vishwesharya@gmail.com>
2022-10-06 14:23:56 -0400

    Edit deploy windows

--- deploy-windows.bat
+++ deploy-windows.bat
@@ -7,7 +7,7 @@ cmd /C %~dp0.venv/Scripts/activate
 python -m venv .venv
 pip install -r requirements.txt
 pip install -e .
-cmd /C schtasks /f /create /tn amatsa-client /tr "%PYTHONPATH% %~dp0src\driver.py" /sc MINUTE /mo %1
+cmd /C schtasks /f /create /tn amatsa-client /tr "%~dp0.venv\Scripts\python.exe %~dp0src\driver.py" /sc MINUTE /mo %1
 
 exit /B 0
 
