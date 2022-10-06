953f702af60b88a69c8a27d85df5581d3dafa83b
Shivangi Chopra <shivangichopra10@gmail.com>
2022-10-06 14:48:27 -0400

    Adding test modules for network

--- tests/test_network.py
+++ tests/test_network.py
@@ -5,6 +5,23 @@ def test_network_init():
     success = True
     try:
         _ = Network()
-    except: #pylint: disable=bare-except
+    except:
         success = False
     assert success, "Failed to initialize Network()"
+
+def test_network_entries():
+    success = True
+    json = {}
+    try:
+        net_obj = Network()
+        net_obj.get_network_info()
+        net_obj.fill_network_info(json)
+        for k, v in json.items():
+            if isinstance(v, str) and str in (None, "unknown"):
+                print(f"Value [{v}] for '{k}' is not expected, expected str.")
+                success = False
+            else:
+                print(f"{k}:{v}, type:{type(v)}")
+    except:
+        success = False
+    assert success, "Failed to collect Network info"
