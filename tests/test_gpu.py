a93cff8e5bb88bc34cfe8bf0f05ce2c36b912fa8
Ali Qureshi <qureshi.ali@outlook.com>
2022-10-07 08:50:21 -0400

    fixing linter issues

--- tests/test_gpu.py
+++ tests/test_gpu.py
@@ -19,7 +19,8 @@ def test_gpu_datatype():
         print("Failed to collect Data metrics")
         success = False
     num = (float, int)
-    data_types = {"gpu_name" : str, "uuid" : str, "serial_number" : num, "load" : num, "total_memory" : num, "memory_used" : num, "memory_free" : num}
+    data_types = {"gpu_name" : str, "uuid" : str, "serial_number" : num, "load" : num, "total_memory" : num,
+    "memory_used" : num, "memory_free" : num}
     for each in data:
         for field in each.keys():
             if not isinstance(each[field], data_types[field]) :
